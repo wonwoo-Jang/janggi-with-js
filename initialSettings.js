0494163d35ede969262a3fd6b732cc521907278c
wonwoo <jww0418@snu.ac.kr>
2022-01-09 21:28:11 +0900

    말이 이동할 수 있는 칸표시 포, 마, 상 제외 만들기 완료

--- initialSettings.js
+++ initialSettings.js
@@ -1,4 +1,4 @@
-function boardClassSetting(i, j, $td){
+function piecesNameSetting(i, j, $td){
   if ([0, 9].includes(i) && [0, 8].includes(j)){
     $td.classList.add("car");
     return;
@@ -29,9 +29,53 @@ function boardClassSetting(i, j, $td){
   } //상차림 하는거 추가하기(지금은 귀마 대 귀마로 설정)
 }
 
+function determinePiecesCountry(i, j, $td, myCountry){
+  let yourCountry;
+  if (myCountry == "cho") yourCountry = "han";
+  else yourCountry = "cho";
+  if ([0].includes(i) && [0, 1, 2, 3, 5, 6, 7, 8].includes(j)){
+    $td.classList.add(yourCountry);
+    return;
+  }
+  if(i == 1 && j == 4){
+    $td.classList.add(yourCountry);
+    return;
+  }
+  if ([2].includes(i) && [1, 7].includes(j)){
+    $td.classList.add(yourCountry);
+    return;
+  }
+  if ([3].includes(i) && [0, 2, 4, 6, 8].includes(j)){
+    $td.classList.add(yourCountry);
+    return;
+  }
+  if ([9].includes(i) && [0, 1, 2, 3, 5, 6, 7, 8].includes(j)){
+    $td.classList.add(myCountry);
+    return;
+  }
+  if(i == 8 && j == 4){
+    $td.classList.add(myCountry);
+    return;
+  }
+  if ([7].includes(i) && [1, 7].includes(j)){
+    $td.classList.add(myCountry);
+    return;
+  }
+  if ([6].includes(i) && [0, 2, 4, 6, 8].includes(j)){
+    $td.classList.add(myCountry);
+    return;
+  }
+}
+
+function addHouseClass(i, j, $td){
+  if([0, 1, 2, 7, 8, 9].includes(i) && [3, 4, 5].includes(j)){
+    $td.classList.add("house");
+  }
+}
+
 function boardContentSetting($td){
   const janggiPieces = $td.classList.value;
   if (janggiPieces) $td.textContent = janggiPieces.slice(0, 1); //나중에 여기 부분 이미지로 변경하기
 }
 
-export {boardClassSetting, boardContentSetting};
+export {piecesNameSetting, boardContentSetting, determinePiecesCountry, addHouseClass};
--- 장기.js
+++ 장기.js
@@ -1,13 +1,14 @@
-import { boardClassSetting, boardContentSetting } from "./initialSettings.js";
+import { piecesNameSetting, boardContentSetting, determinePiecesCountry, addHouseClass } from "./initialSettings.js";
 import { janggiPieces } from "./janggiPieces.js";
 
 const $table = document.querySelector("table");
 const data = [];
 let beforeClickedPiece; //이전에 클릭된 기물이 저장되는 곳(td를 저장)
+let myCountry = "cho"; //내 나라(초, 한) 설정 (유저가 직접 설정하는 거 나중에 추가)
 
 function carValidMove(){
   if (beforeClickedPiece != this){
-    data.flat().forEach((ele, ind) => {
+    data.flat().forEach((ele) => {
       if(ele.classList.contains("valid")) ele.classList.remove("valid");
     })
   } //만약 이전에 클릭된 기물이 아니면, valid를 토글하기 전에 valid를 초기화해줘야 함
@@ -48,7 +49,29 @@ function carValidMove(){
 }
 
 function soldierValidMove(){
+  if (beforeClickedPiece != this){
+    data.flat().forEach((ele) => {
+      if(ele.classList.contains("valid")) ele.classList.remove("valid");
+    })
+  } //만약 이전에 클릭된 기물이 아니면, valid를 토글하기 전에 valid를 초기화해줘야 함
 
+  let upOrDown = 1;
+  if(this.classList.contains(myCountry)){
+    upOrDown = -1;
+  }
+  let rightCheck = data[this.parentNode.rowIndex]?.[this.cellIndex + 1];
+  if(rightCheck && !rightCheck.textContent){
+    rightCheck.classList.toggle("valid");
+  }
+  rightCheck = data[this.parentNode.rowIndex]?.[this.cellIndex - 1];
+  if(rightCheck && !rightCheck.textContent){
+    rightCheck.classList.toggle("valid");
+  }
+  rightCheck = data[this.parentNode.rowIndex + upOrDown]?.[this.cellIndex];
+  if(rightCheck && !rightCheck.textContent){
+    rightCheck.classList.toggle("valid");
+  }
+  beforeClickedPiece = this; //이전에 클릭된 기물에 클릭된 td를 저장
 }
 
 function horseValidMove(){
@@ -64,11 +87,65 @@ function cannonValidMove(){
 }
 
 function kingValidMove(){
-
+  if (beforeClickedPiece != this){
+    data.flat().forEach((ele) => {
+      if(ele.classList.contains("valid")) ele.classList.remove("valid");
+    })
+  } //만약 이전에 클릭된 기물이 아니면, valid를 토글하기 전에 valid를 초기화해줘야 함
+  const x = this.parentNode.rowIndex;
+  const y = this.cellIndex;
+  if (([0, 2, 7, 9].includes(x) && [3, 5].includes(y)) || ([1, 8].includes(x) && y == 4)){
+    [1, 0, -1].forEach((i) => {
+      [1, 0, -1].forEach((j) => {
+        const rightCheck = data[x + i]?.[y + j];
+        if(rightCheck && rightCheck.classList.contains("house") && !rightCheck.textContent && !(i == 0 && j == 0)){
+          rightCheck.classList.toggle("valid");
+        }
+      })
+    })
+    beforeClickedPiece = this; 
+    return; //궁성에서의 궁 위치에 따라 대각선으로 이동할수도, 못할수도 있다!
+  }
+  [1, 0, -1].forEach((i) => {
+    [1, 0, -1].forEach((j) => {
+      const rightCheck = data[x + i]?.[y + j];
+      if(rightCheck && rightCheck.classList.contains("house") && !rightCheck.textContent && Math.abs(i) != Math.abs(j)){
+        rightCheck.classList.toggle("valid");
+      }
+    })
+  })
+  beforeClickedPiece = this; //이전에 클릭된 기물에 클릭된 td를 저장
 }
 
 function scholarValidMove(){
-
+  if (beforeClickedPiece != this){
+    data.flat().forEach((ele) => {
+      if(ele.classList.contains("valid")) ele.classList.remove("valid");
+    })
+  } //만약 이전에 클릭된 기물이 아니면, valid를 토글하기 전에 valid를 초기화해줘야 함
+  const x = this.parentNode.rowIndex;
+  const y = this.cellIndex;
+  if (([0, 2, 7, 9].includes(x) && [3, 5].includes(y)) || ([1, 8].includes(x) && y == 4)){
+    [1, 0, -1].forEach((i) => {
+      [1, 0, -1].forEach((j) => {
+        const rightCheck = data[x + i]?.[y + j];
+        if(rightCheck && rightCheck.classList.contains("house") && !rightCheck.textContent && !(i == 0 && j == 0)){
+          rightCheck.classList.toggle("valid");
+        }
+      })
+    })
+    beforeClickedPiece = this; 
+    return; 
+  }
+  [1, 0, -1].forEach((i) => {
+    [1, 0, -1].forEach((j) => {
+      const rightCheck = data[x + i]?.[y + j];
+      if(rightCheck && rightCheck.classList.contains("house") && !rightCheck.textContent && Math.abs(i) != Math.abs(j)){
+        rightCheck.classList.toggle("valid");
+      }
+    })
+  }) //왕과 완벽하게 동일하게 움직인다.
+  beforeClickedPiece = this; //이전에 클릭된 기물에 클릭된 td를 저장
 }
 
 for(let i = 0; i < 10; i++){
@@ -76,8 +153,10 @@ for(let i = 0; i < 10; i++){
   const $tr = document.createElement("tr");
   for (let j = 0; j < 9; j++){
     const $td = document.createElement("td");
-    boardClassSetting(i, j, $td);
+    piecesNameSetting(i, j, $td);
+    determinePiecesCountry(i, j, $td, myCountry);
     boardContentSetting($td);
+    addHouseClass(i, j, $td);
     if ($td.classList.contains("car"))  $td.addEventListener("click", carValidMove);
     if ($td.classList.contains("soldier")) $td.addEventListener("click", soldierValidMove);
     if ($td.classList.contains("horse"))  $td.addEventListener("click", horseValidMove);
