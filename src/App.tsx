b699e630548f167b1608459105590138415a12c3
Chansol Lim <lchans0319@gmail.com>
2023-02-15 12:18:25 +0900

    ✨ 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
 import './App.css';
 import Header from '@components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from '@pages/main';
+import LoginPage from '@pages/login';
+import SignUpPage from '@pages/signup';
+import GuidePage from '@pages/guide';
+import LearnPage from '@pages/learn';
+import NewsPage from '@pages/news';
+import PlayPage from '@pages/play';
+import PuzzlesPage from '@pages/puzzles';
+import ReviewPage from '@pages/review';
+import SocialPage from '@pages/social';
+import ErrorPage from '@pages/error';
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/guide" element={<GuidePage />} />
+      <Route path="/learn" element={<LearnPage />} />
+      <Route path="/news" element={<NewsPage />} />
+      <Route path="/play" element={<PlayPage />} />
+      <Route path="/puzzles" element={<PuzzlesPage />} />
+      <Route path="/review" element={<ReviewPage />} />
+      <Route path="/social" element={<SocialPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <main>
+          <AppRoutes />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
