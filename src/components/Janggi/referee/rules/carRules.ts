75ab4dbdd15ddcaf935b8c97631d1493ab305f98
Chansol Lim <lchans0319@gmail.com>
2023-02-20 03:25:52 +0900

    ♻️ 차 이동 미리보기 리팩토링

--- src/components/Janggi/referee/rules/carRules.ts
+++ src/components/Janggi/referee/rules/carRules.ts
@@ -3,14 +3,9 @@ import { ROW_LEN } from '@components/Janggi/JanggiBoard';
 import { Piece } from '@models/Piece';
 import { Position } from '@models/Position';
 
-import { Board, CountryType } from '@customTypes/janggi';
+import { Board } from '@customTypes/janggi';
 
-import {
-  isTileOccupied,
-  isTileOccupiedByMyCountry,
-  isTileOccupiedByOpponent,
-  PALACE,
-} from './generalRules';
+import { isTileOccupied, isTileOccupiedByOpponent, PALACE } from './generalRules';
 
 const getLinearMoves = (
   dx: number,
@@ -22,7 +17,7 @@ const getLinearMoves = (
   const linearMoves: Position[] = [];
   const { x: currX, y: currY } = car.position;
 
-  for (let i = 1; i < maxDistance; i++) {
+  for (let i = 1; i <= maxDistance; i++) {
     const position: Position = new Position(currX + i * dx, currY + i * dy);
     if (!isTileOccupied(position, board)) {
       // destination is empty
@@ -47,46 +42,44 @@ export const getPossibleCarMoves = (car: Piece, board: Board): Position[] => {
   const dy = [0, 0, -1, 1];
   const DIRECTION_NUM = 4;
   for (let i = 0; i < DIRECTION_NUM; i++) {
-    const normalMoves = getLinearMoves(dx[i], dy[i], ROW_LEN, car, board);
+    // 이거 가로 볼 때는 column_len으로 전달하는 게 맞긴 함
+    const normalMoves = getLinearMoves(dx[i], dy[i], ROW_LEN - 1, car, board);
     possibleMoves.push(...normalMoves);
   }
 
   // can move diagonally inside the palace
-  let palaceDx = 0;
-  let palaceDy = 0;
+  // 여기도 여전히 하드코딩한 느낌이 조금 있긴 한데 나중에 생각하자
+  const palaceDx = [1, 1, -1, -1];
+  const palaceDy = [1, -1, 1, -1];
+  const palaceCornerPositions: Position[][] = [
+    [PALACE.cho.bottomLeft, PALACE.han.topRight],
+    [PALACE.cho.bottomRight, PALACE.han.topLeft],
+    [PALACE.cho.topLeft, PALACE.han.bottomRight],
+    [PALACE.cho.topRight, PALACE.han.bottomLeft],
+  ];
+  let palaceIndex = -1;
+
+  for (let i = 0; i < 4; i++) {
+    const isAtPalaceCorner = palaceCornerPositions[i].some(corner =>
+      corner.isSamePosition(car.position),
+    );
+    if (isAtPalaceCorner) {
+      palaceIndex = i;
+      break;
+    }
+  }
+
   if (
-    car.position.isSamePosition(PALACE.cho.bottomLeft) ||
-    car.position.isSamePosition(PALACE.han.topRight)
-  ) {
-    palaceDx = 1;
-    palaceDy = 1;
-  } else if (
-    car.position.isSamePosition(PALACE.cho.bottomRight) ||
-    car.position.isSamePosition(PALACE.han.topLeft)
-  ) {
-    palaceDx = 1;
-    palaceDy = -1;
-  } else if (
-    car.position.isSamePosition(PALACE.cho.topLeft) ||
-    car.position.isSamePosition(PALACE.han.bottomRight)
-  ) {
-    palaceDx = -1;
-    palaceDy = 1;
-  } else if (
-    car.position.isSamePosition(PALACE.cho.topRight) ||
-    car.position.isSamePosition(PALACE.han.bottomLeft)
-  ) {
-    palaceDx = -1;
-    palaceDy = -1;
-  } else if (
     car.position.isSamePosition(PALACE.cho.center) ||
     car.position.isSamePosition(PALACE.han.center)
   ) {
     console.log('Center');
   }
 
-  const palaceMoves = getLinearMoves(palaceDx, palaceDy, 3, car, board);
-  possibleMoves.push(...palaceMoves);
+  if (palaceIndex !== -1) {
+    const palaceMoves = getLinearMoves(palaceDx[palaceIndex], palaceDy[palaceIndex], 2, car, board);
+    possibleMoves.push(...palaceMoves);
+  }
 
   return possibleMoves;
 };
