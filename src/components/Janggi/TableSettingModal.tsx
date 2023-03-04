c18051ca9df79a22a11acb61d74cf7ede481e91a
Chansol Lim <lchans0319@gmail.com>
2023-03-04 15:45:28 +0900

    ✨ 상대방 상차림 보여주기

--- src/components/Janggi/TableSettingModal.tsx
+++ src/components/Janggi/TableSettingModal.tsx
@@ -20,17 +20,24 @@ export default function TableSettingModal({
   return (
     <div className={styles.tableSettingModal}>
       <div className={styles.message}>
-        <h3>상차림 선택</h3>
+        <div className={styles.header}>
+          <h3>상차림 선택</h3>
+          <div className={styles.opponentOption}>
+            {TABLE_SETTING_OPTIONS[0].map((p, i) => (
+              <div style={{ backgroundImage: `url('images/${CountryType.HAN}_${p}.png')` }} key={i} />
+            ))}
+          </div>
+        </div>
         <span>대국 시작시 상/마의 위치를 선택합니다.</span>
       </div>
       <div className={styles.options}>
         {TABLE_SETTING_OPTIONS.map((option, i) => (
           <div className={styles.option} key={i}>
-            {option.map(type => (
+            {option.map(p => (
               <div
                 className={styles.piece}
-                style={{ backgroundImage: `url('images/${myCountry}_${type}.png')` }}
-                key={`${type}${i}`}
+                style={{ backgroundImage: `url('images/${myCountry}_${p}.png')` }}
+                key={`${p}${i}`}
               />
             ))}
           </div>
